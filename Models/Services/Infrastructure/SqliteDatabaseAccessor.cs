4dae1c61625c82022ac49d01ece726520e5c7854
KatiaLosignore <katialos.kl@gmail.com>
2024-07-05 10:31:15 +0200

    Added SqliteParameter for SqlInjection

--- Models/Services/Infrastructure/SqliteDatabaseAccessor.cs
+++ Models/Services/Infrastructure/SqliteDatabaseAccessor.cs
@@ -9,13 +9,32 @@ namespace People.Models.Services.Infrastructure
 {
     public class SqliteDatabaseAccessor : IDatabaseAccessor
     {
-        public DataSet Query(string query)
+        public DataSet Query(FormattableString formattableQuery)
         {
+            //dato che SqliteCommand accetta come parametro query una stringa
+            //devo per forza convertire la FormattableString in string
+            var queryArguments = formattableQuery.GetArguments();//recupero gli argomenti della query  -> torna un array di SqliteParameter
+            var sqliteParameters = new List<SqliteParameter>();//creo una lista di argomenti
+            //per ogni argomento recuperato dalla query
+            for (var i = 0; i < queryArguments.Length; i++)
+            {
+                //Il costruttore di SqliteParameter vuole un valore numerico come string e il valore da iniettare nella query
+                var parameter = new SqliteParameter(i.ToString(), queryArguments[i]);
+                sqliteParameters.Add(parameter);
+                queryArguments[i] = "@" + i;
+
+            }
+
+            string query = formattableQuery.ToString();
+
             using (var conn = new SqliteConnection("Data Source=Data/MyPersons.db"))
             {
                 conn.Open();
                 using (var cmd = new SqliteCommand(query, conn))
                 {
+                    //tramite il metodo AddRange aggiungo tutti i parametri recuperati dalla query
+                    cmd.Parameters.AddRange(sqliteParameters);
+                    
                     using (var reader = cmd.ExecuteReader())
                     {
                         var dataSet = new DataSet();
